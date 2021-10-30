f99da4be3d360eb7ce40b93ec86fc3ae1fd9fab1
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-10-30 20:15:04 +0900

    [Add] Added DELETE query

--- main.go
+++ main.go
@@ -35,6 +35,15 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	_, err = Db.Exec(cmd, "hanako", 19)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = Db.Exec(cmd, "abe", 78)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("\n==================================================================\n")
 
 	// UPDATE
 	cmd = "UPDATE persons SET age = ? WHERE name = ?"
@@ -42,6 +51,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	fmt.Println("\n==================================================================\n")
 
 	// SELECT
 	cmd = "SELECT * FROM persons WHERE name = ?"
@@ -56,11 +66,53 @@ func main() {
 		}
 	}
 	fmt.Println("p.Name, p.Age =>", p.Name, p.Age)
+	fmt.Println("\n==================================================================\n")
+
+	// SELECT All
+	cmd = "SELECT * FROM persons"
+	rows, _ := Db.Query(cmd) // クエリ結果を返す。今回は条件にマッチしたレコードを全て返す。
+	defer rows.Close()
+
+	// personsというスライスを作って、データを1個1個追加していってる。
+	var persons []Person
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil{
+			log.Println(err)
+		}
+		persons = append(persons, p)
+	}
+	fmt.Println("persons =>", persons)
+
+	for i, person := range persons {
+		fmt.Println("i, person =>", i, person)
+	}
+	fmt.Println("\n==================================================================\n")
+
 
 	// DELETE
-	// cmd = "INSERT INTO persons (name, age) VALUES(?, ?)"
-	// _, err = Db.Exec(cmd, "hanako", 19)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	cmd = "DELETE FROM persons WHERE name = ?"
+	_, err = Db.Exec(cmd, "tarou")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// DELETEできたか確認
+	cmd = "SELECT * FROM persons"
+	rows, _ = Db.Query(cmd) // クエリ結果を返す。今回は条件にマッチしたレコードを全て返す。
+	defer rows.Close()
+
+	persons = []Person{}
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil{
+			log.Println(err)
+		}
+		persons = append(persons, p)
+	}
+	fmt.Println("persons =>", persons)
+	fmt.Println("\n==================================================================\n")
+
 }
