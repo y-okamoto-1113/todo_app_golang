eea92fb333abe83ee3099e9c9940703ac145d4e0
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-11-01 00:07:14 +0900

    [Add] Added Loader function to load config before main() calls

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
 type ConfigList struct {
 	Port      string // サーバーport番号
 	SQLDriver string // SQLドライバの名前
@@ -8,3 +14,21 @@ type ConfigList struct {
 }
 
 var Config ConfigList
+
+func init() {
+	LoadConfig()
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module todo_app_golang
 
 go 1.17
+
+require gopkg.in/go-ini/ini.v1 v1.63.2
+
+require github.com/stretchr/testify v1.7.0 // indirect
