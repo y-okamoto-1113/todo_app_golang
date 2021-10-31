5c8f1cbb1172fad7dc3ed31b68f6941452c035fb
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-11-01 01:19:51 +0900

    [Add] Created User Table

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require gopkg.in/go-ini/ini.v1 v1.63.2
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_app_golang/config"
+	"todo_app_golang/app/models"
 )
 
 func main() {
-	fmt.Println("config.Config.Port =>", config.Config.Port)
-	fmt.Println("config.Config.SQLDriver =>", config.Config.SQLDriver)
-	fmt.Println("config.Config.DbName =>", config.Config.DbName)
-	fmt.Println("config.Config.LogFile =>", config.Config.LogFile)
+	// fmt.Println("config.Config.Port =>", config.Config.Port)
+	// fmt.Println("config.Config.SQLDriver =>", config.Config.SQLDriver)
+	// fmt.Println("config.Config.DbName =>", config.Config.DbName)
+	// fmt.Println("config.Config.LogFile =>", config.Config.LogFile)
 
-	log.Println(1111)
+	// log.Println(1111)
+
+	fmt.Println(models.Db)
 }
