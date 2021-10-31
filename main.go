a9e231dfb508fb2f95ce969c420ac87aa0b69d76
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-11-01 01:06:43 +0900

    [Add] Set the log file destination and format.

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"todo_app_golang/config"
 )
 
 func main() {
-	fmt.Println(config.Config)
+	fmt.Println("config.Config.Port =>", config.Config.Port)
+	fmt.Println("config.Config.SQLDriver =>", config.Config.SQLDriver)
+	fmt.Println("config.Config.DbName =>", config.Config.DbName)
+	fmt.Println("config.Config.LogFile =>", config.Config.LogFile)
+
+	log.Println(1111)
 }
