13def6865077c812deed3f1935d15c9fb51002c0
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-11-05 01:39:05 +0900

    [Update] Commonized the HTML layout.

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func TopHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "Hello World!!!") // 第2引数はHTMLに渡せる。HTML側で`{{.}}`で展開できる
+	generateHTML(w, "Hello World from generateHTML function", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -2,9 +2,29 @@ package controllers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"todo_app_golang/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	fmt.Println("=================================================================")
+	fmt.Println("filenames in generateHTML() =>", filenames)
+
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	fmt.Println("files in generateHTML() =>", files)
+
+	// Must は引数のファイルをあらかじめキャッシュする。エラーの場合は Panic になる。
+	templates := template.Must(template.ParseFiles(files...))
+	fmt.Println("templates in generateHTML() =>", templates)
+	// defineを使って定義したテンプレートを使う場合は、テンプレート名を明示的に指定する必要がある。
+	// 第3引数はテンプレートに渡す（展開する）値を渡す。
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
